682f19e3d66dcff9fc57313f372e393647fe8da6
Elly Lesanjore <lesanjorelly@gmail.com>
2023-07-31 01:28:19 -0700

    list_len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,13 @@
+#include <stddef.h>
+#include "lists.h"
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+	const list_t  *current = h;
+	while(current != NULL)
+	{
+		count++;
+		current = current -> next;
+	}
+	return (count);
+}
